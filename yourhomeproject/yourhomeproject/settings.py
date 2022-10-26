7b0c4de686a1fb856d9b6625e111581eaa704805
cutesuyeon <ansy311@naver.com>
2022-10-26 21:41:32 +0900

    사진
    사진 변경

--- yourhomeproject/yourhomeproject/settings.py
+++ yourhomeproject/yourhomeproject/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@b2z3-+8i)3mdcy&zcgo#nl+a)#7jv&1a#=22e-esx@jcf39_!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
