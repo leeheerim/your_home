6af397b68b98d622727cfe6d5351393d2663fad6
leeheerim <hriiim1211@gmail.com>
2022-10-26 22:15:48 +0900

    retry

--- yourhomeproject/yourhomeproject/settings.py
+++ yourhomeproject/yourhomeproject/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@b2z3-+8i)3mdcy&zcgo#nl+a)#7jv&1a#=22e-esx@jcf39_!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
@@ -119,7 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR,'static'),)
+STATICFILES_DIRS = (os.path.join(BASE_DIR, ''),)
 
 
 # Default primary key field type
