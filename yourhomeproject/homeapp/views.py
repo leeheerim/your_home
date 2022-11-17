3f8736c6ae6f57626e686317589ef6681421b38a
cutesuyeon <ansy311@naver.com>
2022-11-17 21:57:21 +0900

    푸시푸시베이베

--- yourhomeproject/homeapp/views.py
+++ yourhomeproject/homeapp/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from .models import table
+from django.db.models import Q
+
 
 # Create your views here.
 def home(request):
+    data = table.objects.all()
+    print(data)
+    
     return render(request,'home.html')
